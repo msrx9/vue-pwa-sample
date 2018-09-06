6d13973c35f9cd8dc6dc6882d006ca2d6d008a22
msrx9 <msrx9g@gmail.com>
2018-09-07 00:26:34 +0900

    Add docs

---
+++ docs/precache-manifest.66b9d96a82d60450e38b86b610afd52c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-pwa-sample/robots.txt"
+  },
+  {
+    "revision": "83ff2ee32db5f00d7925",
+    "url": "/vue-pwa-sample/js/chunk-vendors.bb3a6f6d.js"
+  },
+  {
+    "revision": "ab55949661aa1f94de6a",
+    "url": "/vue-pwa-sample/js/app.3a9d1c56.js"
+  },
+  {
+    "revision": "30e79dd562ec119b0079043c5a248206",
+    "url": "/vue-pwa-sample/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-pwa-sample/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ab55949661aa1f94de6a",
+    "url": "/vue-pwa-sample/css/app.f5fba5f5.css"
+  }
+];
